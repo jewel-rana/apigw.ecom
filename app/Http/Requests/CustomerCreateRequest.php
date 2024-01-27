f1bacf64dd0e34e540680ec629f46f7deaa15ac2
Jewel Rana <jewelrana.dev@gmail.com>
2024-01-27 17:17:09 +0600

    Customer create request done

--- app/Http/Requests/CustomerCreateRequest.php
+++ app/Http/Requests/CustomerCreateRequest.php
@@ -6,23 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CustomerCreateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'mobile' => 'nullable|string|unique:customers,mobile',
+            'email' => 'required|email|unique:customers,email',
+            'password' => 'required|string|min:6|max:18|same:password_confirm',
+            'gender' => 'required|string|in:male,female',
+            'address' => 'nullable|string'
         ];
     }
 }
