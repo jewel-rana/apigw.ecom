7288f76434a3ec09415209e52a66d77f68949468
Jewel Rana <jewelrana.dev@gmail.com>
2024-01-27 13:47:56 +0600

    customer login done

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,14 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
+    use HasRoles, HasApiTokens, HasFactory, Notifiable;
+
     protected $fillable = [
-        'user_id',
         'gender',
         'created_by',
-        'updated_by'
+        'updated_by',
+        'name',
+        'mobile',
+        'email',
+        'password',
+        'status',
+        'is_system'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'email_verified_at',
+        'type',
+        'is_system'
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
