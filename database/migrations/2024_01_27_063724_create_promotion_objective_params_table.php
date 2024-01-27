da3874cf448e80600db8ed0b4d666d77b8eb8a54
Jewel Rana <jewelrana.dev@gmail.com>
2024-01-27 13:56:26 +0600

    Json value

--- database/migrations/2024_01_27_063724_create_promotion_objective_params_table.php
+++ database/migrations/2024_01_27_063724_create_promotion_objective_params_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('key');
             $table->string('label');
             $table->string('placeholder')->nullable();
-            $table->json('items');
+            $table->text('items');
             $table->enum('status', ['Active', 'Inactive'])->index();
             $table->timestamps();
         });
