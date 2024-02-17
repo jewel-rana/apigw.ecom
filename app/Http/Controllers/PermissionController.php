709155e3ac98b7849959b1c757d241ab3a14cd8b
Jewel Rana <jewelrana.dev@gmail.com>
2024-02-17 20:23:42 +0600

    Permission list permission fix

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Permission;
-use App\Models\Role;
 use App\Services\PermissionService;
 
 class PermissionController extends Controller
@@ -18,12 +16,5 @@ class PermissionController extends Controller
     public function index()
     {
         return response()->success($this->permissionService->format());
-    }
-
-    public function callAction($method, $parameters)
-    {
-        if($this->authorize($method, Permission::class)) {
-            return parent::callAction($method, $parameters);
-        }
     }
 }
