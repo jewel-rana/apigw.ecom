d1ca46f20f3bf09fb17c669a1400cecd60c8b2f9
Jewel Rana <jewelrana.dev@gmail.com>
2024-02-26 17:24:07 +0600

    testing mail configuration

--- app/Notifications/OtpNotification.php
+++ app/Notifications/OtpNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class OtpNotification extends Notification implements ShouldQueue
+class OtpNotification extends Notification
 {
     use Queueable;
 
