30012b28d7582ef9b4326314196ac74227b415cd
Jewel Rana <jewelrana.dev@gmail.com>
2024-03-11 23:36:48 +0600

    Gateway response save

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -64,6 +64,7 @@ class PaymentService
             $gatewayResponse = app(GatewayService::class)->execute($gateway, $payment);
 
             if (is_object($gatewayResponse) && $gatewayResponse->paymentID) {
+                $payment->update(['gateway_response' => (array) $gatewayResponse]);
                 if ($gatewayResponse->transactionStatus == AppConstant::BKASH_COMPLETED) {
                     $payment->update(['status' => AppConstant::PAYMENT_SUCCESS]);
                     return response()->success(['order_id' => $payment->order_id]);
