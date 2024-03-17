87024b4dd83100bef4a21dbe1f9bbb5231288599
Jewel Rana <jewelrana.dev@gmail.com>
2024-03-17 12:30:59 +0600

    color bug fix

--- config/colors.php
+++ config/colors.php
@@ -2,12 +2,10 @@
 
 return [
     'order' => [
-        'colors' => [
-            'pending' => '#FFC700',
-            'publish' => '#3182CE',
-            'refunded' => '#FF3938',
-            'cancelled' => '#FF3938',
-            'complete' => '#69CB1C'
-        ]
+        'pending' => '#FFC700',
+        'publish' => '#3182CE',
+        'refunded' => '#FF3938',
+        'cancelled' => '#FF3938',
+        'complete' => '#69CB1C'
     ]
 ];
