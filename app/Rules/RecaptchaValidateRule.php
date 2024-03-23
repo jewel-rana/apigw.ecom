34c4a255bcacc218910d198412c6a16a6e98a6c6
Jewel Rana <jewelrana.dev@gmail.com>
2024-03-24 00:04:35 +0600

    Recaptcha disabled from config

--- app/Rules/RecaptchaValidateRule.php
+++ app/Rules/RecaptchaValidateRule.php
@@ -11,13 +11,15 @@ class RecaptchaValidateRule implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         try {
-            $response = Http::get("https://www.google.com/recaptcha/api/siteverify",[
-                'secret' => env('GOOGLE_RECAPTCHA_SECRET'),
-                'response' => $value
-            ]);
+            if(config('scopes.captcha_enabled')) {
+                $response = Http::get("https://www.google.com/recaptcha/api/siteverify", [
+                    'secret' => env('GOOGLE_RECAPTCHA_SECRET'),
+                    'response' => $value
+                ]);
 
-            if (!($response->json()["success"] ?? false)) {
-                $fail(__('You are not a human.'));
+                if (!($response->json()["success"] ?? false)) {
+                    $fail(__('You are not a human.'));
+                }
             }
         } catch (\Exception $exception) {
             $fail(__('Re-Captcha verification failed'));
--- config/scopes.php
+++ config/scopes.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'captcha_enabled' => env('GOOGLE_RECAPTCHA_ENABLED', true),
     'permissions' => [
         'user-list' => 'User list',
         'user-create' => 'User Create',
