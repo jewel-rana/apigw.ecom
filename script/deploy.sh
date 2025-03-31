732a5b719c60e1cb88aac9fa2291033483771f15
Jewel Rana <jewelrana.dev@gmail.com>
2025-03-31 12:16:45 +0600

    Fix deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,18 +8,18 @@ pushd /var/www/html/apigw.prokash.io
 
 git checkout .
 git pull origin master
-yes | php8.2  /usr/local/bin/composer install
-php8.2 artisan migrate
-php8.2 artisan module:migrate
+yes | php composer install
+php artisan migrate
+php artisan module:migrate
 
 sudo chmod -R 777 storage
 sudo chmod -R 777 bootstrap/cache
-sudo php8.2 artisan route:clear
-sudo php8.2 artisan view:clear
-sudo php8.2 artisan cache:clear
-sudo php8.2 artisan config:clear
-sudo php8.2 artisan optimize:clear
-sudo php8.2 artisan queue:restart
+sudo php artisan route:clear
+sudo php artisan view:clear
+sudo php artisan cache:clear
+sudo php artisan config:clear
+sudo php artisan optimize:clear
+sudo php artisan queue:restart
 
 popd
 sudo chown -Rf nginx:kartat /var/www/html/apigw.prokash.io
@@ -28,7 +28,7 @@ sudo chmod -Rf 777 /var/wwww/html/apigw.prokash.io/storage
 sudo chmod 660 /var/www/html/apigw.prokash.io/storage/oauth-p*
 sudo chmod -Rf 777 /var/www/html/apigw.prokash.io/bootstrap/cache
 
-#sudo systemctl reload php8.2-fpm
+#sudo systemctl reload php-fpm
 #sudo nginx -s reload
 
 
