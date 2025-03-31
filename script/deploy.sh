7aa0c9febc685816b55f89c6a4a7dc31c0663978
Jewel Rana <jewelrana.dev@gmail.com>
2025-03-31 12:21:27 +0600

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,27 +2,26 @@
 set -e
 
 echo "Deploying application to dev server"
-echo dev-admin
 
 pushd /var/www/html/apigw.prokash.io
 
 git checkout .
 git pull origin master
-yes | composer install
+composer install
 php artisan migrate
 php artisan module:migrate
 
 sudo chmod -R 777 storage
 sudo chmod -R 777 bootstrap/cache
-sudo php artisan route:clear
-sudo php artisan view:clear
-sudo php artisan cache:clear
-sudo php artisan config:clear
-sudo php artisan optimize:clear
-sudo php artisan queue:restart
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan config:clear
+php artisan optimize:clear
+php artisan queue:restart
 
 popd
-sudo chown -Rf nginx:kartat /var/www/html/apigw.prokash.io
+sudo chown -Rf nginx:nginx /var/www/html/apigw.prokash.io
 sudo chmod -Rf 770 /var/www/html/apigw.prokash.io
 sudo chmod -Rf 777 /var/wwww/html/apigw.prokash.io/storage
 sudo chmod 660 /var/www/html/apigw.prokash.io/storage/oauth-p*
