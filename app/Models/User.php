d56f7f425d28a39a479e1a638428e206c2f26e4d
Jewel Rana <jewelrana.dev@gmail.com>
2025-06-25 18:29:48 +0600

    Fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,6 +69,11 @@ class User extends Authenticatable implements MustVerifyEmail
         return ucfirst($value);
     }
 
+    public function getStatusAttribute($value): string
+    {
+        return ucwords($value);
+    }
+
     public function getPermissions(): array
     {
         return $this->hasRole('admin') ?
