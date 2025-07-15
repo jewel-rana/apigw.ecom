ca09e31fcaff9081ef1e3aa5aa7a0c8f4dfab98c
Jewel Rana <jewelrana.dev@gmail.com>
2025-07-15 17:40:10 +0600

    Fix

--- Modules/Banner/Entities/Banner.php
+++ Modules/Banner/Entities/Banner.php
@@ -75,7 +75,7 @@ class Banner extends Model
                 'updated_by' => $this->updatedBy?->only(['id', 'name']),
                 'attachment' => $this->media->attachment ?? null,
             ]
-            + $this->only(['id', 'title', 'medium_text', 'small_text', 'position', 'remarks', 'status']);
+            + $this->only(['id', 'title', 'medium_text', 'small_text', 'position', 'remarks', 'banner_url', 'status']);
     }
 
     public static function boot(): void
