2dca083353c82d9f9512004c54890126d5a51116
Jewel Rana <jewelrana.dev@gmail.com>
2025-07-29 12:48:20 +0600

    COOKIE HANDLD PROPERLY

--- app/Http/Middleware/ApiCookieMiddleware.php
+++ app/Http/Middleware/ApiCookieMiddleware.php
@@ -18,21 +18,21 @@ class ApiCookieMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        Log::info('guest_unique_id in request: ' . $request->cookie('guest_unique_id'));
-
-        $response = $next($request);
-
         $guestId = $request->cookie('guest_unique_id', $request->header('X-GUEST-ID'));
 
         if (!$guestId) {
             $guestId = CommonHelper::generateUniqueUUID();
-
-            $request->merge(['guest_unique_id' => $guestId]);
-
-            $response->withCookie(
-                cookie('guest_unique_id', $guestId, 60 * 24 * 30)
-            );
         }
+
+        Log::info('guest_unique_id in request: ' . $guestId);
+
+        $request->merge(['guest_unique_id' => $guestId]);
+
+        $response = $next($request);
+
+        $response->withCookie(
+            cookie('guest_unique_id', $guestId, 60 * 24 * 30)
+        );
 
         return $response;
     }
