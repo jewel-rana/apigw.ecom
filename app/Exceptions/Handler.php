d981cadc5484923319b3e264cc3bb0fe17d714ac
Jewel Rana <jewelrana.dev@gmail.com>
2026-01-13 16:49:50 +0600

    fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -113,6 +113,7 @@ class Handler extends ExceptionHandler
 
         // Fallback for all other exceptions
         $this->renderable(function (Throwable $e, $request) {
+            dd($e, $request->all());
             return response()->json([
                 'status' => false,
                 'message' => 'Server error.',
